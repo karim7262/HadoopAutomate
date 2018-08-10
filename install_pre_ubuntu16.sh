18618bc12675ce0424b3cacf82a754be263bb47a
mujahed85 <mujahed.mca@gmail.com>
2018-08-10 13:11:17 +0530

    hugepage transparent working with method4

--- install_pre_ubuntu16.sh
+++ install_pre_ubuntu16.sh
@@ -34,9 +34,7 @@ function check_pkgMgr()
 function check_java_install()
 {
 	#Check Java if not present install
-	##################################
-	# if [ ! type java ];
-	# then
+	################################## 
 	if ! [ -x "$(command -v java)" ]; then
 			if [ -x "$(command -v yum)" ]; then
 					echo "Installing through yum..Update Require...."
@@ -67,29 +65,29 @@ function check_ntp_install()
 function root_reservedspace()
 {
 	echo "Root Reserved Space"
-	# sudo mkfs.ext4 -m 0 /dev/xvda1
-	# lsblk
-	# sudo tune2fs -m 0 /dev/xvda1
+	sudo mkfs.ext4 -m 0 /dev/xvda1
+	lsblk
+	sudo tune2fs -m 0 /dev/xvda1
 }
 
 function set_swappiness()
 {
 	echo "setting swappiness"
-	# echo "Current Swappiness is: " && cat /proc/sys/vm/swappiness
-	# sudo sysctl vm.swappiness=0
-	# sudo sysctl -p
-	# echo "Updated Swappiness is: " && cat /proc/sys/vm/swappiness
+	echo "Current Swappiness is: " && cat /proc/sys/vm/swappiness
+	sudo sysctl vm.swappiness=1
+	sudo sysctl -p
+	echo "Updated Swappiness is: " && cat /proc/sys/vm/swappiness
 }	
 
 function set_ipv6()
 {
 	echo "disable ipv6"
-	# echo "Current IPV6 Status are(all,default,lo) : " && cat /proc/sys/net/ipv6/conf/all/disable_ipv6 && cat /proc/sys/net/ipv6/conf/default/disable_ipv6 && cat /proc/sys/net/ipv6/conf/lo/disable_ipv6
-	# sudo sysctl net.ipv6.conf.all.disable_ipv6=1
-	# sudo sysctl net.ipv6.conf.default.disable_ipv6=1
-	# sudo sysctl net.ipv6.conf.lo.disable_ipv6=1 
-	# sudo sysctl -p
-	# echo "Updated IPV6 Status are(all,default,lo) : " && cat /proc/sys/net/ipv6/conf/all/disable_ipv6 && cat /proc/sys/net/ipv6/conf/default/disable_ipv6 && cat /proc/sys/net/ipv6/conf/lo/disable_ipv6
+	echo "Current IPV6 Status are(all,default,lo) : " && cat /proc/sys/net/ipv6/conf/all/disable_ipv6 && cat /proc/sys/net/ipv6/conf/default/disable_ipv6 && cat /proc/sys/net/ipv6/conf/lo/disable_ipv6
+	sudo sysctl net.ipv6.conf.all.disable_ipv6=1
+	sudo sysctl net.ipv6.conf.default.disable_ipv6=1
+	sudo sysctl net.ipv6.conf.lo.disable_ipv6=1 
+	sudo sysctl -p
+	echo "Updated IPV6 Status are(all,default,lo) : " && cat /proc/sys/net/ipv6/conf/all/disable_ipv6 && cat /proc/sys/net/ipv6/conf/default/disable_ipv6 && cat /proc/sys/net/ipv6/conf/lo/disable_ipv6
 }
 
 function set_iptables()
@@ -109,18 +107,47 @@ function set_hugepageTransparent()
 
 	
 	#Method 2
+	# echo "Method2 for Hugepage Transparent"
 	# sudo sed -i '/exit 0/d' /etc/rc.local
+	# echo "Removed exit 0"
 	# sudo su -c 'cat >>/etc/rc.local <<EOL
 	# if test -f /sys/kernel/mm/transparent_hugepage/enabled; then
-	  # echo never > /s	ys/kernel/mm/transparent_hugepage/enabled
+	  # echo never > /sys/kernel/mm/transparent_hugepage/enabled
 	# fi
 	# if test -f /sys/kernel/mm/transparent_hugepage/defrag; then
 	   # echo never > /sys/kernel/mm/transparent_hugepage/defrag
 	# fi
 	# exit 0
 	# EOL'
-	# sudo -i
 	# source /etc/rc.local
+	# echo "added multiple if blocks"
+	# sudo -i
+	# echo "after sudo -i"
+
+	# echo "after source rc.local"
+	# exit
+	# echo "after exit called"
+
+	##Method 3
+    # sudo sed -i '/exit 0/d' /etc/rc.local
+    ##sudo su
+    # sudo echo 'if test -f /sys/kernel/mm/transparent_hugepage/enabled; then' >> /etc/rc.local
+    # sudo echo '  echo never > /sys/kernel/mm/transparent_hugepage/enabled' >> /etc/rc.local
+    # sudo echo 'fi' >> /etc/rc.local
+    # sudo echo 'if test -f /sys/kernel/mm/transparent_hugepage/defrag; then' >> /etc/rc.local
+    # sudo echo '   echo never > /sys/kernel/mm/transparent_hugepage/defrag' >> /etc/rc.local
+    # sudo echo 'fi' >> /etc/rc.local
+    # sudo echo 'exit 0' >> /etc/rc.local
+    # source /etc/rc.local
+
+    # Method 4
+    if test -f /sys/kernel/mm/transparent_hugepage/enabled; then
+        echo never > /sys/kernel/mm/transparent_hugepage/enabled
+    fi
+    if test -f /sys/kernel/mm/transparent_hugepage/defrag; then
+        echo never > /sys/kernel/mm/transparent_hugepage/defrag
+    fi
+    echo "All Lines added now exiting"
 }
 
 
@@ -132,11 +159,11 @@ function set_hugepageTransparent()
 function install_apt_java()
 {
 	echo "Installing java through apt-get"
-	# sudo apt-get install -y python-software-properties debconf-utils
-	# sudo add-apt-repository -y ppa:webupd8team/java
-	# sudo apt-get update
-	# echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-	# sudo apt-get install -y oracle-java8-installer
+	sudo apt-get install -y python-software-properties debconf-utils
+	sudo add-apt-repository -y ppa:webupd8team/java
+	sudo apt-get update
+	echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+	sudo apt-get install -y oracle-java8-installer
 }
 
 # echo "**********************"
@@ -149,10 +176,10 @@ function install_apt_java()
 # set_swappiness
 # echo "**********************"
 # set_ipv6
-# echo "**********************"
+# echo "*****iptables wont remove any issues*****************"
 # set_iptables
 # echo "**********************"
-# set_hugepageTransparent
+set_hugepageTransparent
 # echo "**********************"
 # check_ntp_install
 # echo "**********************"
